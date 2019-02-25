0a2777dd5ce14b2617b4c9ca6ddeb3b7b8d94c38
m.dmitriev <makc-dmv@yandex.ru>
2019-02-25 11:12:54 +0500

    Удалил повтор в конфиге, вкл. кеш в babel-loader v.2.1.7

--- prod.webpack.config.js
+++ prod.webpack.config.js
@@ -3,9 +3,5 @@ const common = require('./common.webpack.config.js')
 
 module.exports = merge(common, {
   mode: 'production',
-  output: {
-    filename: 'script.js',
-    chunkFilename: 'vendor.bundle.js'
-  },
   devtool: 'source-map'
 })
