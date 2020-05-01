3907418b883b164b9e7f51137074a64c60074bbc
Максим <ctpagateji@yandex.ru>
2020-05-01 22:39:06 +0500

    update gh-pages

--- sbp-config/tasks/deploy.js
+++ sbp-config/tasks/deploy.js
@@ -1,5 +1,5 @@
 import { src } from 'gulp';
-import ghpages from 'gulp-gh-pages';
+import ghpages from 'gulp4-gh-pages';
 
 export function deployTask () {
   return src(['./build/**/*', '!build/robots.txt'])
