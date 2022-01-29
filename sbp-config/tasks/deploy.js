51f1da125e25a03447cb75d3b39447b8c9420b94
Максим Дмитриев <makc-dmv@yandex.ru>
2022-01-29 11:37:14 +0500

    fix

--- sbp-config/tasks/deploy.js
+++ sbp-config/tasks/deploy.js
@@ -3,5 +3,5 @@ import ghpages from 'gulp4-gh-pages';
 
 export function deployTask () {
   return src(['./build/**/*', '!build/robots.txt'])
-    .pipe(ghpages({ branch: 'gh-pages' }))
+    .pipe(ghpages({ branch: 'gh-pages' }));
 }
