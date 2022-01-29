00882d003f2a4bb4fcc81c9327e1808da3844b13
Максим Дмитриев <makc-dmv@yandex.ru>
2022-01-29 16:29:25 +0500

    fixed js style eslint

--- sbp-config/tasks/zipArchive.js
+++ sbp-config/tasks/zipArchive.js
@@ -2,9 +2,9 @@ import { src, dest } from 'gulp';
 import plumber from 'gulp-plumber';
 import zip from 'gulp-zip';
 
-export function zipArchive() {
+export function zipArchive () {
   return src('dist/**/*.*')
     .pipe(plumber())
     .pipe(zip('archive.zip'))
-    .pipe(dest('dist'))
+    .pipe(dest('dist'));
 }
