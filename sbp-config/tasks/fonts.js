5bd005311662dbf742b8e7825a6a0bc9b0057adf
Максим Дмитриев <makc-dmv@yandex.ru>
2022-01-31 10:42:57 +0500

    fix style lint

--- sbp-config/tasks/fonts.js
+++ sbp-config/tasks/fonts.js
@@ -3,7 +3,7 @@ import { src, dest } from 'gulp';
 import plumber from 'gulp-plumber';
 import path from '../path.js';
 
-export function fontsTask() {
+export function fontsTask () {
   return src(path.src.fonts)
     .pipe(plumber())
     .pipe(dest(path.build.fonts));
