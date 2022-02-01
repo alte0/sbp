e549d366c8f5918f36a34343ea5703bf59845f1a
Максим Дмитриев <makc-dmv@yandex.ru>
2022-02-01 10:14:40 +0500

    fix sassTask

--- sbp-config/tasks/sass.js
+++ sbp-config/tasks/sass.js
@@ -6,17 +6,18 @@ import flags from '../flags';
 import plumber from 'gulp-plumber';
 import gulpif from 'gulp-if';
 // import sass from 'gulp-sass';
-import dartSass from 'sass';
 import gulpSass from 'gulp-sass';
-const sass = gulpSass(dartSass);
 import sassGlob from 'gulp-sass-glob';
 import sassVars from 'gulp-sass-variables';
-//import stylelint from 'gulp-stylelint';
+// import stylelint from 'gulp-stylelint';
 import postcss from 'gulp-postcss';
-import autoprefixer from 'autoprefixer';
+// import autoprefixer from 'autoprefixer';
 import sourcemaps from 'gulp-sourcemaps';
 import cssnano from 'gulp-cssnano';
 import bs from 'browser-sync';
+import dartSass from 'sass';
+
+const sass = gulpSass(dartSass);
 
 export function sassTask () {
   return (src(path.src.css)
@@ -24,20 +25,19 @@ export function sassTask () {
     .pipe(gulpif(flags.bs, sourcemaps.init()))
     .pipe(gulpif(flags.minify, sassVars({ $minify: true })))
     .pipe(sassGlob())
-    /*.pipe(
+    /* .pipe(
       stylelint({
         reporters: [
           { formatter: 'string', console: true }
         ]
       })
-    )*/
+    ) */
     .pipe(sass().on('error', sass.logError))
     .pipe(postcss())
     .pipe(gulpif(flags.minify, cssnano()))
     .pipe(gulpif(!flags.watch, cssnano()))
     .pipe(gulpif(flags.bs, sourcemaps.write('.')))
-    .pipe(gulpif(flags.watch, dest(path.dev.css)))
-    .pipe(gulpif(!flags.watch, dest(path.build.css)))
+    .pipe(dest(path.build.css))
     .pipe(gulpif(flags.bs, bs.stream()))
   );
 }
